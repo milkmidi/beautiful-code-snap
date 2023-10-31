e4db76c92fadb6108d09948dc38923659aac7405
Michael0520 <po2319987@gmail.com>
2023-11-01 00:05:39 +0800

    feat: 🎸 first init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
