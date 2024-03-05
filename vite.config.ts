eb1cd7bb1fec3d7d1258244789e025b4ad81b2f1
Michael Lo <po2319987@gmail.com>
2024-03-06 04:26:56 +0800

    Add million compiler plugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-
+import million from "million/compiler";
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
