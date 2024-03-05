1d9a0974d0b61fc87052bcd00377e02d55416a3d
Michael Lo <po2319987@gmail.com>
2024-03-06 05:03:16 +0800

    Revert "Add ecmaVersion to .eslintrc.cjs"
    This reverts commit 51e28b3ff2bb3a736daa9263b7592e28fcda39c1.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import million from "million/compiler";
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
